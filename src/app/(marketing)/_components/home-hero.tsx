e7225a6bb5a98cd5ea64129d077e45037713df7a
aka <aaronkaltman@gmail.com>
2024-01-05 08:58:32 -0500

    marketing design

---
+++ src/app/(marketing)/_components/home-hero.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+export default function HomeHero() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <h1>hi</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import HomeHero from "./(marketing)/_components/home-hero";
 
 export default function Home() {
   return (
     <main>
+      <HomeHero />
       <h1>Skill Swap Hub</h1>
     </main>
   );
