884eceb561297a7ec79ed8b55c59e5606bb8b7a3
aka <aaronkaltman@gmail.com>
2024-01-05 12:07:17 -0500

    footer

--- src/components/ui/my_ui/footer.tsx
+++ src/components/ui/my_ui/footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto px-2 py-4 bg-slate-800 text-white">
-      <small>hi this is the footer</small>
+    <footer className="mt-auto px-6 py-4 bg-slate-800 text-white">
+      <p className="text-xl text-center">Trade Skills with Community</p>
     </footer>
   );
 }
